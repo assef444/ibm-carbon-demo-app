833f69341819316610c227a2378429659295ce1d
assef <fernandoassef@hotmail.com>
2025-09-11 15:56:04 -0600

    build: instala sass dependency

---
+++ src/app/src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <p>
+                this is my IBM Carbon Demo App
+            </p>
+        </>
+    )
+}
+
+export default App
--- src/app/src/main.tsx
+++ src/app/src/main.tsx
@@ -1,7 +1,11 @@
-import { StrictMode } from 'react'
+import * as React from 'react';
 import { createRoot } from 'react-dom/client'
+import App from './app/App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+const root = document.getElementById('root');
+
+createRoot(root).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
