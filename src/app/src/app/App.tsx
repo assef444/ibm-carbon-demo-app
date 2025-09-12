cb553b64aac4b9e1bfc04e6eb8b69ad7decc9299
ferna <fernandoassef@hotmail.com>
2025-09-12 15:56:40 -0600

    feat: agrega componentes Header y SideNav

--- src/app/src/app/App.tsx
+++ src/app/src/app/App.tsx
@@ -1,15 +1,101 @@
-import { Theme, Button, Grid, Column } from '@carbon/react'
-import { Bee } from '@carbon/icons-react'
+import { Header, 
+    HeaderName, 
+    SideNav, 
+    SideNavItems,
+    SideNavMenu,
+    SideNavDivider,
+    SideNavMenuItem,
+    SideNavLink, 
+    Content,
+    Theme,
+    HeaderGlobalBar,
+    HeaderGlobalAction,
+ } from '@carbon/react';
+import { 
+    UserAvatar, 
+    Notification, 
+    LetterAa, 
+    LetterBb, 
+    LetterCc,
+    LetterDd
+} from '@carbon/icons-react';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
     return (
-        <Theme theme="g10">
-            <Grid>
-                <Column sm={4} md={8} lg={16}>
-                    <h1>IBM Carbon Demo App</h1>
-                    <Button kind="primary" renderIcon={Bee}>Hello Carbon!</Button>
-                </Column>
-            </Grid>
-        </Theme>
-    )
+        <>
+            <Theme theme="g90">
+                <Header>
+                    <HeaderName href="#" prefix="IBM">
+                        Carbon Demo App
+                    </HeaderName>
+                    <HeaderGlobalBar>
+                        <HeaderGlobalAction
+                            aria-label="Notifications"
+                            badgeCount={2}
+                            id="notification-button"
+                            onBlur={() => { }}
+                            onClick={() => { }}
+                            onKeyDown={() => { }}
+                            tooltipAlignment="center"
+                        >
+                            <Notification size={20} />
+                        </HeaderGlobalAction>
+                        <HeaderGlobalAction
+                            aria-label="Profile"
+                            onClick={() => { }}
+                            tooltipAlignment="end"
+                        >
+                            <UserAvatar size={20} />
+                        </HeaderGlobalAction>
+                    </HeaderGlobalBar>
+                </Header>
+                <SideNav isFixedNav expanded={true} isChildOfHeader={false} aria-label="Side navigation">
+                    <SideNavItems>
+                        <SideNavMenu renderIcon={LetterAa} title="Collapsable Section A">
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page A-0
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page A-1
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page A-2
+                            </SideNavMenuItem>
+                        </SideNavMenu>
+                        <SideNavMenu renderIcon={LetterBb} title="Collapsable Section B">
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page B-0
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page B-1
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page B-2
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page B-3
+                            </SideNavMenuItem>
+                        </SideNavMenu>
+                        <SideNavLink renderIcon={LetterCc} href="https://cassia.solutions">
+                            Page C
+                        </SideNavLink>
+                        <SideNavDivider />
+                        <SideNavMenu renderIcon={LetterDd} title="Collapsable Section D">
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page D-0
+                            </SideNavMenuItem>
+                            <SideNavMenuItem href="https://cassia.solutions">
+                                Page D-1
+                            </SideNavMenuItem>
+                        </SideNavMenu>
+                    </SideNavItems>
+                </SideNav>
+
+                <Content>
+                    <Outlet /> {/* this is where app routes get loaded */}
+                </Content>
+            </Theme>
+        </>
+    );
 }
